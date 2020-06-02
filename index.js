344198b5e295d8b9c3254cfae01116a8ade44eda
Victoria <vickychang.3843@gmail.com>
2020-06-02 18:28:37 -0500

    Fix unexpeted token error

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const { JSEncrypt } = require('jsencrypt');
-​
+
 const decrypt = new JSEncrypt();
 const encrypt = new JSEncrypt();
-​
+
 function getPrivKey() {
   return fs.readFileSync(path.resolve(process.env.PATH_PRIKEY, process.env.FILENAME_PRIKEY), 'utf8');
 }
 function getPubKey() {
   return fs.readFileSync(path.resolve(process.env.PATH_PUBKEY, process.env.FILENAME_PUBKEY), 'utf8');
 }
-​
+
 const privkey = getPrivKey();
 const pubkey = getPubKey();
 decrypt.setPrivateKey(privkey);
 encrypt.setPublicKey(pubkey);
-​
+
 const originalValue = 'Este dato será encriptado';
 console.log(': ----------------------------');
 console.log('originalValue', originalValue);
